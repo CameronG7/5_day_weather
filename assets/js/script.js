e55dc2ca7a0602763efffda8886be83c723b1a93
CameronG7 <122698132+CameronG7@users.noreply.github.com>
2023-03-20 16:44:29 -0700

    html base

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+const currWeatherEl = $("#currentWeather");
+const currTempEl = $("#currTemp");
+const currWindEl = $("#currWind");
+const currHumEl = $("#currHum");
+const recentEl = $("#recentSearch");
+

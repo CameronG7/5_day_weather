e2618b15f61c120dfc2f7d5a2bc5800a7e6bc426
CameronG7 <122698132+CameronG7@users.noreply.github.com>
2023-03-20 21:41:19 -0700

    js started

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,33 @@
+// element variables 
 const currWeatherEl = $("#currentWeather");
 const currTempEl = $("#currTemp");
 const currWindEl = $("#currWind");
 const currHumEl = $("#currHum");
 const recentEl = $("#recentSearch");
+const cityEl = $("#cityInput");
+const dayTempEl = $("#dayTemp");
+const dayWindEl = $("#dayWind");
+const dayHumEl = $("#dayHum"); 
+const searchButtonEl = $("#searchButton");
 
+
+const MyKey = config.WEATHER_KEY
+
+
+
+
+searchButtonEl.on("click",grabCoord);
+
+function grabCoord(event) // grab json data CAMERON YOU NEED TO FINISH GRABBING COORDS
+{
+    const UserCity = cityEl.val()
+    console.log(UserCity);
+    const coordURL = `https://api.openweathermap.org/data/2.5/weather?q=${UserCity}&appid=${MyKey}`;
+    fetch (coordURL)
+        .then(function(response){
+            return response.json()
+        })
+        .then(function(data){
+            console.log(data)
+        });
+};
